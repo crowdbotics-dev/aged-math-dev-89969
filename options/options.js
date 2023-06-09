9179a696ef3d97c785cbbbadb7e557e394f6346b
Crowdbotics <engineering@crowdbotics.com>
2023-06-09 08:06:13 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "aged-math-dev-89969",
+  url: "https://aged-math-dev-89969.botics.co",
+  api: "https://aged-math-dev-89969.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
